df5baaa78fe9166bbab7bf3f8ed59cd9d7402b6d
Sahriar Mahmud <smrafsu97@gmail.com>
2024-03-16 05:27:13 +0100

    added several design, about to perform functionability

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       <Header></Header>
-      <div className='md:flex'>
-      <Blogs></Blogs>
-      <Bookmarks></Bookmarks>
+      <div className='md:flex max-w-7xl mx-auto'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
       </div>
     </>
   )
